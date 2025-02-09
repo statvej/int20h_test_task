031f5f108c4597ae93abfdd60220fd476ac3aca1
yarutiun <y.arutiunofff@gmail.com>
2025-02-09 15:49:21 +0100

    minor change and config

--- frontend/frontend-app/src/App.jsx
+++ frontend/frontend-app/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LogIn from './components/LogIn';
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 function App() {
-
+  const clientId = import.meta.env.VITE_CLIENT_ID;
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <GoogleOAuthProvider clientId={clientId}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signin" element={<LogIn />} />
+          <Route path="/verify" element={<LogIn />} />
+          <Route path="/main" element={<LogIn />} />
+        </Routes>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   )
 }
 
---
+++ frontend/frontend-app/src/components/LogIn.jsx
@@ -0,0 +1,23 @@
+import '../App.css'
+// import { GoogleLogin } from '@react-oauth/google';
+// import google from '/google.png'
+
+const LogIn = () => {
+  // const responseMessage = (response) => {
+  //   console.log(response);
+  // };
+  
+  // const errorMessage = (error) => {
+  //     console.log(error);
+  // };
+
+  return (
+    <div className='flex flex-col min-h-screen'>
+      <header className='w-full h-[92px] flex justify-between items-center px6 bg-gray-100 shadow-md'>
+        <h1 className='text-xl fo'></h1>
+      </header>
+    </div>
+  )
+}
+
+export default LogIn;
