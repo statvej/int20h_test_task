d0a3ccb8b4e06a9d1f4d92432c5c174ddcfc5b37
kilchenk <kirill220420054004@gmail.com>
2025-02-09 16:41:26 +0100

    merge

--- frontend/frontend-app/src/components/LogIn.jsx
+++ frontend/frontend-app/src/components/LogIn.jsx
@@ -12,9 +12,10 @@ const LogIn = () => {
   // };
 
   return (
-    <div className='flex flex-col min-h-screen'>
-      <header className='w-full h-[92px] flex justify-between items-center px6 bg-gray-100 shadow-md'>
-        <h1 className='text-xl fo'></h1>
+    <div className='flex flex-col'>
+      <header className='w-full h-24 flex justify-between items-center px6 bg-gray-100 shadow-md'>
+        <h1 className='text-xl font-semibold'>QUIZQUIZ</h1>
+        <button className='px-4 py-2 bg-blue-400 text-white rounded-lg'>Sign Up</button>
       </header>
     </div>
   )
