3c4746feb882ea6c324a953693026308aa4ec505
kilchenk <kirill220420054004@gmail.com>
2025-02-10 18:28:51 +0100

    Front: delete unnecessary comments

--- frontend/frontend-app/src/components/GameInfo.jsx
+++ frontend/frontend-app/src/components/GameInfo.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import '../App.css';
 import Header from "./Header";
 import { UserRound } from "lucide-react";
@@ -46,7 +47,6 @@ const GameInfo = () => {
                     </div>
                     <p className="ml-2 text-gray-600 text-xl">{rating}/5</p>
                 </div>
-                {/* Блок "Recommended" */}
                 {/* <div className="mt-6">
                     <div className="flex space-x-20 mt-2">
                     {[1, 2, 3, 4, 5].map((game) => (
@@ -67,34 +67,3 @@ const GameInfo = () => {
 };
 
 export default GameInfo;
-
-
-// import '../App.css';
-// import Header from "./Header";
-// import { useSelector } from "react-redux";
-// import { UserRound } from "lucide-react";
-
-// const GameInfo = () => {
-//     const user = useSelector((state) => state.user);
-
-//     return (
-//         <div>
-//             <Header flag='1' />
-//             <div className="mt-25 px-10 pt-6">
-//             <div className="flex justify-self-start gap-4">
-//                 {/* <img src={user.profilePic} alt="Profile" className="w-16 h-16 rounded-full" /> */}
-//                 <UserRound className="h-20 w-20 rounded-full" />
-//                 <div className="flex text-left flex-col gap-1 justify-self-start">
-//                     <h2 className="text-xl font-semibold">Game Name</h2>
-//                     <a className=" text-gray-600">5 questions</a>
-//                     <button className="ml-auto bg-black text-white px-11 py-0 rounded">Play</button>
-//                 </div>
-//             </div>
-//             {/* <div className="px-20 flex justify-self-start"> */}
-//             {/* </div> */}
-//         </div>
-//         </div>
-//     )
-// };
-
-// export default GameInfo;
