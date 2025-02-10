3f8111212b69e9492436ac7ffcbe04f7cdc13474
yarutiun <y.arutiunofff@gmail.com>
2025-02-10 19:01:23 +0100

    fallback added for 404 routes and error boundaries

--- frontend/frontend-app/src/App.jsx
+++ frontend/frontend-app/src/App.jsx
@@ -7,20 +7,24 @@ import Main from './components/Main';
 import UserProfile from './components/UserProfile';
 import GameInfo from './components/GameInfo';
 import QuizCreationForm from './components/CreateQuiz';
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/signin" element={<SignIn/>} />
-          <Route path="/verify" element={<Verify />} />
-          <Route path="/info" element={<GameInfo />} />
-          <Route path="/" element={<Main />} />
-          <Route path="/profile" element={<UserProfile />}/>
-          <Route path="/create" element={<QuizCreationForm/>} />
-        </Routes>
-      </BrowserRouter>
+      <ErrorBoundary fallback={<div>Something went wrong!</div>}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/signin" element={<SignIn/>} />
+            <Route path="/verify" element={<Verify />} />
+            <Route path="/info" element={<GameInfo />} />
+            <Route path="/" element={<Main />} />
+            <Route path="/profile" element={<UserProfile />}/>
+            <Route path="/create" element={<QuizCreationForm/>} />
+            <Route path="*" element={<div>404 Error Not Found!</div>} />
+          </Routes>
+        </BrowserRouter>
+    </ErrorBoundary>
   )
 }
 
--- frontend/frontend-app/src/components/Header.jsx
+++ frontend/frontend-app/src/components/Header.jsx
@@ -8,65 +8,80 @@ const Header = (props) => {
   const navigate = useNavigate();
   const user = useSelector((state) => state.user);
   console.log(props.userPic);
-  if (props.flag === '0') {
+  if (props.flag === "0") {
     return (
-      <header className="fixed top-0 left-0 flex w-full h-16 justify-between items-center bg-gray-900 shadow-md px-4"> 
+      <header className="fixed top-0 left-0 flex w-full h-16 justify-between items-center bg-gray-900 shadow-md px-4">
         <div className="flex items-center">
           <Link to="/" className="cursor-pointer">
             <h1 className="text-white font-bold text-2xl mr-4">QuizQuizQuiz</h1>
           </Link>
         </div>
-  
+
         <div className="flex items-center">
-          <Link to="/signin" className="cursor-pointer" > 
+          <Link to="/signin" className="cursor-pointer">
             <button className="flex items-center justify-center bg-gray-800 hover:bg-gray-700 text-white rounded-md px-4 py-2">
-              <CircleFadingPlus className="text-white h-6 w-6 mr-2"/>
+              <CircleFadingPlus className="text-white h-6 w-6 mr-2" />
               Sign Up
             </button>
           </Link>
         </div>
       </header>
-    );  
-  } else if (props.flag === '2') {
+    );
+  } else if (props.flag === "2") {
     return (
-    <header className="fixed top-0 left-0 flex w-full h-16 justify-between items-center bg-gray-900 shadow-md px-4"> 
-      <div className="flex items-center">
-        <Link to="/" className="cursor-pointer" >
+      <header className="fixed top-0 left-0 flex w-full h-16 justify-between items-center bg-gray-900 shadow-md px-4">
+        <div className="flex items-center">
+          <Link to="/" className="cursor-pointer">
             <h1 className="text-white font-bold text-2xl mr-4">QuizQuizQuiz</h1>
-        </Link>
-      </div>
+          </Link>
+        </div>
 
-      <div className="flex items-center">
-        <Link to="/login" className="cursor-pointer" > 
-          <button className="flex items-center justify-center bg-gray-800 hover:bg-gray-700 text-white rounded-md px-4 py-2">
-            <CircleFadingPlus className="text-white h-6 w-6 mr-2" />
-            Sign in
-          </button>
-        </Link>
-      </div>
-    </header>
+        <div className="flex items-center">
+          <Link to="/login" className="cursor-pointer">
+            <button className="flex items-center justify-center bg-gray-800 hover:bg-gray-700 text-white rounded-md px-4 py-2">
+              <CircleFadingPlus className="text-white h-6 w-6 mr-2" />
+              Sign in
+            </button>
+          </Link>
+        </div>
+      </header>
     );
   } else {
     return (
-    <header className="fixed top-0 left-0 flex w-full h-16 justify-between items-center bg-gray-900 shadow-md px-4"> 
-      <div className="flex items-center">
-        <Link to="/" className="cursor-pointer" > 
-          <h1 className="text-white font-bold text-2xl mr-4">QuizQuizQuiz</h1>
-        </Link> 
-      </div>
+      <header className="fixed top-0 left-0 flex w-full h-16 justify-between items-center bg-gray-900 shadow-md px-4">
+        <div className="flex items-center">
+          <Link to="/" className="cursor-pointer">
+            <h1 className="text-white font-bold text-2xl mr-4">QuizQuizQuiz</h1>
+          </Link>
+        </div>
 
-      <div className="flex items-center"> 
-        <button className="flex items-center justify-center bg-gray-800 hover:bg-gray-700 text-white rounded-md px-4 py-2">
-          <CircleFadingPlus className="text-white h-6 w-6 mr-2" />
-          Create Your Own Quiz
-        </button>
-        <button onClick={() => navigate('/profile')} className="cursor-pointer ml-auto">
-        {!user.picture ? <UserRound className="text-white h-6 w-6 ml-4"/> : <img src={user.picture} alt="User" className="h-6 w-6 ml-4 rounded-full"/>}
-        </button>
-      </div>
-    </header>
-  );
-}
+        <div className="flex items-center">
+          <button className="flex items-center justify-center bg-gray-800 hover:bg-gray-700 text-white rounded-md px-4 py-2">
+            <CircleFadingPlus className="text-white h-6 w-6 mr-2" />
+            Create Your Own Quiz
+          </button>
+          <button
+            onClick={() => navigate("/profile")}
+            className="cursor-pointer ml-auto"
+          >
+            {!user.picture ? (
+              <button onClick={() => navigate("/create")}>
+                <UserRound className="text-white h-6 w-6 ml-4" />
+              </button>
+            ) : (
+              <button>
+                <img
+                  src={user.picture}
+                  alt="User"
+                  className="h-6 w-6 ml-4 rounded-full"
+                />
+              </button>
+            )}
+          </button>
+        </div>
+      </header>
+    );
+  }
 };
 
 export default Header;
