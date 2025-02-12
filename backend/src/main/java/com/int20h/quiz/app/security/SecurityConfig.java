1ca688c21d2833b89743f0488026e6e76cc65161
Fedir Staryk <fedir.staryk@t-systems.com>
2025-02-12 17:04:49 +0100

    feat: allow more accesses

--- backend/src/main/java/com/int20h/quiz/app/security/SecurityConfig.java
+++ backend/src/main/java/com/int20h/quiz/app/security/SecurityConfig.java
@@ -30,7 +30,8 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
-      .csrf(csrf -> csrf.disable())
+      .cors(AbstractHttpConfigurer::disable)
+      .csrf(AbstractHttpConfigurer::disable)
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authorizeHttpRequests(auth -> auth
         .requestMatchers("/auth/**").permitAll()  // Allow public access
