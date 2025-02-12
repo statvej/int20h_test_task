49586727d3861128067dc97893f4216e05766438
Fedir Staryk <fedir.staryk@t-systems.com>
2025-02-12 18:37:20 +0100

    feat: implemented basic review crud

--- backend/src/main/java/com/int20h/quiz/app/services/ReviewService.java
+++ backend/src/main/java/com/int20h/quiz/app/services/ReviewService.java
@@ -1,10 +1,14 @@
 package com.int20h.quiz.app.services;
 
+import com.int20h.quiz.app.entities.Review;
 import com.int20h.quiz.app.mappers.ReviewMapper;
 import com.int20h.quiz.app.model.ReviewDto;
 import com.int20h.quiz.app.repositories.QuestRepository;
 import com.int20h.quiz.app.repositories.ReviewRepository;
 import com.int20h.quiz.app.repositories.UserRepository;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +28,13 @@ public class ReviewService {
     review.setQuest(quest);
     final var returnReview = reviewRepository.save(review);
     return reviewMapper.entityToDto(returnReview);
+  }
+
+  public List<ReviewDto> getReviewsByQuestId(UUID id) {
+    final var reviews = reviewRepository.findAllByQuestId(id).orElseThrow(() -> new RuntimeException("Review not found"));
+    return reviews.stream()
+      .map(reviewMapper::entityToDto)
+      .collect(Collectors.toList());
   }
 
 }
