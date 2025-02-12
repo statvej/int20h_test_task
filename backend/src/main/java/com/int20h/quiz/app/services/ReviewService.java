33be6bbbf919d275c05f539dcd3d3a918fd9e754
Fedir Staryk <fedir.staryk@t-systems.com>
2025-02-12 17:49:39 +0100

    fix: added create review

--- backend/src/main/java/com/int20h/quiz/app/services/ReviewService.java
+++ backend/src/main/java/com/int20h/quiz/app/services/ReviewService.java
@@ -1,5 +1,6 @@
 package com.int20h.quiz.app.services;
 
+import com.int20h.quiz.app.mappers.ReviewMapper;
 import com.int20h.quiz.app.model.ReviewDto;
 import com.int20h.quiz.app.repositories.QuestRepository;
 import com.int20h.quiz.app.repositories.ReviewRepository;
@@ -13,12 +14,16 @@ public class ReviewService {
   private final ReviewRepository reviewRepository;
   private final UserRepository userRepository;
   private final QuestRepository questRepository;
+  private final ReviewMapper reviewMapper;
 
   public ReviewDto createReview(ReviewDto reviewDto) {
     final var user = userRepository.findByEmail(reviewDto.getAuthorEmail()).orElseThrow(() -> new RuntimeException("User not found"));
     final var quest = questRepository.findById(reviewDto.getQuestId()).orElseThrow(() -> new RuntimeException("Quest not found"));
-    reviewRepository.save(ReviewDto.toEntity(reviewDto, user, quest));
-    return null;
+    var review = reviewMapper.dtoToEntity(reviewDto);
+    review.setUser(user);
+    review.setQuest(quest);
+    final var returnReview = reviewRepository.save(review);
+    return reviewMapper.entityToDto(returnReview);
   }
 
 }
